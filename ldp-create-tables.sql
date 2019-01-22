1bc849e88febd3c01006390e63eaa4f14a6c06c8
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2019-01-22 05:51:06 +0000

    Minor clean up

--- ldp-create-tables.sql
+++ ldp-create-tables.sql
@@ -1,3 +1,6 @@
+-- Requires superuser:
+-- CREATE EXTENSION IF NOT EXISTS tablefunc;
+
 CREATE TABLE groups (
     id          UUID NOT NULL PRIMARY KEY,
     group_name  TEXT NOT NULL UNIQUE,
@@ -26,24 +29,23 @@ CREATE TABLE tmp_loans_locations (
 );
 
 CREATE VIEW users_dim AS
-    SELECT u.id, u.username, u.active, g.group_name
-        FROM users u
-	    LEFT JOIN groups g ON u.patron_group_id = g.id;
+SELECT u.id, u.username, u.active, g.group_name
+    FROM users u
+        LEFT JOIN groups g ON u.patron_group_id = g.id;
 
 CREATE VIEW locations_dim AS
-    SELECT 'id-' || replace(lower(tll.location_name), ' ', '-') location_id,
-           tll.location_name
-        FROM (
-            SELECT DISTINCT location_name
-	        FROM tmp_loans_locations
-	    ) tll;
+SELECT 'id-' || replace(lower(tll.location_name), ' ', '-') location_id,
+       tll.location_name
+    FROM (
+        SELECT DISTINCT location_name FROM tmp_loans_locations
+    ) tll;
 
 CREATE VIEW loans_fact AS
-    SELECT l.id,
-           l.user_id,
-	   'id-' || replace(lower(tll.location_name), ' ', '-') location_id,
-           l.loan_date
-        FROM loans l
-	    JOIN tmp_loans_locations tll ON l.id = tll.loan_id;
+SELECT l.id,
+       l.user_id,
+       'id-' || replace(lower(tll.location_name), ' ', '-') location_id,
+       l.loan_date
+    FROM loans l
+        JOIN tmp_loans_locations tll ON l.id = tll.loan_id;
 
 
