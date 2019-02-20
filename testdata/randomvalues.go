42f366c10ee22075de918d7b400743331fa90cfd
Roman Ruiz-Esparza <rjruizes@ncsu.edu>
2019-02-20 16:55:41 -0500

    rename old random value code

--- testdata/randomvalues.go
+++ testdata/randomvalues.go
@@ -7,18 +7,18 @@ import (
 	"time"
 )
 
-// generate sample data that are random, based on either a list of values or a range/step
+// generate random values based on either a list of values or a range/step
 //   - A list of values would be like the values in groups.groupname:
 //       [faculty, staff, undergraduate student, graduate student]
 //   - A range/step would something like a for-loop: range from 10-100, step 5 = 10, 15, 20, etc.
 
 // outputSize corresponds to the number of rows in the table that you want to generate
 
-// generateRandom doesn't know anything about the data.
+// makeRandomValues doesn't know anything about the data.
 // It just wants to know how much data to generate and how many possible values there are.
 
 // Streams integers, each integer being between 0 and the given max
-func GenerateRandomValues(maxValue, outputSize int, chnl chan int) {
+func makeRandomValues(maxValue, outputSize int, chnl chan int) {
 	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < outputSize; i++ {
 		chnl <- rand.Intn(maxValue)
@@ -26,7 +26,7 @@ func GenerateRandomValues(maxValue, outputSize int, chnl chan int) {
 	close(chnl)
 }
 
-type Generator struct {
+type ValueParams struct {
 	MaxValue   int
 	OutputSize int
 	Channel    chan int
@@ -36,33 +36,33 @@ type ChannelResponse struct {
 	Ok    bool
 }
 
-// Creates 3 example generators, streams responses from each
-func threeGenerators() {
-	firstGen := Generator{2, 10, make(chan int)}
-	secondGen := Generator{4, 10, make(chan int)}
-	thirdGen := Generator{6, 10, make(chan int)}
-	generators := []Generator{firstGen, secondGen, thirdGen}
-	for _, gen := range generators {
-		go GenerateRandomValues(gen.MaxValue, gen.OutputSize, gen.Channel)
+// Example of streaming 3 random values and outputting a string with them
+func threeInstances() {
+	valueType1 := ValueParams{2, 10, make(chan int)}
+	valueType2 := ValueParams{4, 10, make(chan int)}
+	valueType3 := ValueParams{6, 10, make(chan int)}
+	allValueTypes := []ValueParams{valueType1, valueType2, valueType3}
+	for _, valueType := range allValueTypes {
+		go makeRandomValues(valueType.MaxValue, valueType.OutputSize, valueType.Channel)
 	}
 	for {
-		// Get a response from each generator
+		// 1) Get a response from each generator
 		var responses []ChannelResponse
-		for _, gen := range generators {
+		for _, valueType := range allValueTypes {
 			resp := ChannelResponse{}
-			newVal, ok := <-gen.Channel
+			newVal, ok := <-valueType.Channel
 			resp.Value = newVal
 			resp.Ok = ok
 			// In this example, the streamed response is added to an array
 			// In actual usage, we'd want to use the response (e.g. forming a JSON record)
 			responses = append(responses, resp)
 		}
-		// Form a string from the responses
-		var genValues []int
+		// 2) Form a string from the responses
+		var values []int
 		var channelClosed = false
 		for _, resp := range responses {
 			if resp.Ok {
-				genValues = append(genValues, resp.Value)
+				values = append(values, resp.Value)
 			} else {
 				channelClosed = true
 				break
@@ -72,7 +72,7 @@ func threeGenerators() {
 			break
 		} else {
 			delim := " "
-			stringArray := strings.Trim(strings.Join(strings.Fields(fmt.Sprint(genValues)), delim), "[]")
+			stringArray := strings.Trim(strings.Join(strings.Fields(fmt.Sprint(values)), delim), "[]")
 			fmt.Println("Received", stringArray)
 		}
 	}
